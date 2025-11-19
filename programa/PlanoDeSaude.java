ce851839a24245b2c588a0824ea872c135dd0046
Gabriel Lischt Saldanha <pet0080409@aluno.firjansenaisesi.com.br>
2025-11-19 20:27:55 -0300

    Adiciona classes principais

---
+++ programa/PlanoDeSaude.java
@@ -0,0 +1,20 @@
+package programa;
+
+public class PlanoDeSaude {
+    private int id_consulta;
+    private String nome;
+
+    public int getId_consulta() {
+        return id_consulta;
+    }
+    public void setId_consulta(int id_consulta) {
+        this.id_consulta = id_consulta;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
